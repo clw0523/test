d9514198b8b98bd2cdf9078b7fc62d19eee367d9
clw0523 <331830245@qq.com>
2018-03-06 09:16:33 +0800

    分支00

--- src/main/java/com/laview/Laview.java
+++ src/main/java/com/laview/Laview.java
@@ -5,6 +5,8 @@ public class Laview {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("i am laview2 33");
+		
+		System.out.println("0000");
 	}
 
 }
